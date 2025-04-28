7379ea343bb39ba6284d637cd74b44142a6375c9
DMould123 <david.mould123@yahoo.com>
2025-04-28 12:26:12 +0200

    Created a contexts folder. Added UserContext with basic user state management and authentication methods

---
+++ app/contexts/UserContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+export function UserProvider({ children }) {
+  const [user, setUser] = useState();
+
+  async function login(username, password) {}
+
+  async function register(username, password) {}
+
+  async function logout() {}
+
+  return (
+    <UserContext.Provider value={{ user, login, register, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
